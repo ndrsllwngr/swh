172014ed0c15e977291277ed9c65e874406e8195
griase94 <andy.griesbeck@gmail.com>
2020-09-18 15:44:46 +0200

    Add servo util

---
+++ servo_util.py
@@ -0,0 +1,46 @@
+from machine import Pin, PWM
+from time import sleep
+
+DUTY_MIN = 20
+DUTY_MAX = 123
+SLEEP = 0.1
+
+servoPin = Pin(26, Pin.OUT)
+servo = PWM(servoPin, freq=50)
+servo.deinit()
+servo = PWM(servoPin, freq=50)
+
+
+# position needs to be between 0.0 and 1.0
+def rotate(servo, position):
+    print("desired position: "+ str(position))
+    current_duty = servo.duty()
+    if(current_duty>DUTY_MAX):
+        current_duty = DUTY_MAX
+    print("curr duty: " + str(current_duty))
+    new_duty = int(DUTY_MIN + position * (DUTY_MAX - DUTY_MIN))
+    print("new duty: " + str(new_duty))
+
+    step = 1 if current_duty < new_duty else -1
+
+    for i in range(current_duty, new_duty+1, step):
+        servo.duty(i)
+        sleep(SLEEP)
+
+
+rotate(servo, 1.0)
+sleep(1.0)
+rotate(servo, 0.5)
+sleep(1.0)
+rotate(servo, 1.0)
+sleep(1.0)
+rotate(servo, 0.5)
+sleep(1.0)
+rotate(servo, 1.0)
+sleep(1.0)
+rotate(servo, 0.5)
+sleep(1.0)
+rotate(servo, 1.0)
+sleep(1.0)
+rotate(servo, 0.5)
+sleep(1.0)
