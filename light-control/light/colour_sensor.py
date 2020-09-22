20d5af4d868ca50ad53810d74b006a124a3c4d16
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2020-09-22 15:15:33 +0200

    Refactor colourSensor, reformat ringLed

--- light-control/light/colour_sensor.py
+++ light-control/light/colour_sensor.py
@@ -2,102 +2,91 @@ from machine import Pin, ADC
 from time import sleep
 import math
 
-ldr = ADC(Pin(35))
-ldr.atten(ADC.ATTN_11DB)
 
-ledRed = Pin(12, Pin.OUT)
-ledGreen = Pin(13, Pin.OUT)
-ledBlue = Pin(14, Pin.OUT)
+class DIY_COLOUR_SENSOR():
 
-ledArray = [ledRed, ledGreen, ledBlue]
-colourArray = [0,0,0]
-#whiteArray = [0,0,0]
-whiteArray = [2141, 3246, 2536]
-#blackArray = [0,0,0]
-blackArray = [654, 1622, 1239]
+    def __init__(self, ldrPin=35, ledRedPin=12, ledGreenPin=13, ledBluePin=14):
+        self.ldr = ADC(Pin(ldrPin))
+        self.ldr.atten(ADC.ATTN_11DB)
 
-balanceSet = False
-avgRead = 0.0
+        self.ledRed = Pin(ledRedPin, Pin.OUT)
+        self.ledGreen = Pin(ledGreenPin, Pin.OUT)
+        self.ledBlue = Pin(ledBluePin, Pin.OUT)
 
-def checkBalance():
-  if balanceSet == False:
-    setBalance()
-  
+        self.ledArray = [self.ledRed, self.ledGreen, self.ledBlue]
+        self.colourArray = [0, 0, 0]
+        self.whiteArray = [2141, 3246, 2536]  # [0,0,0]
+        self.blackArray = [654, 1622, 1239]  # [0,0,0]
 
-def setBalance():
-  print("setBalance - white")
-  sleep(5)
-  for i in range(0,3):
-    ledArray[i].value(1)
-    sleep(0.5)
-    getReading(5) 
-    global whiteArray
-    whiteArray[i] = avgRead
-    ledArray[i].value(0)
-    sleep(0.5)
-  
-  print("setBalance - black")
-  sleep(5)
-  for i in range(0,3):
-    ledArray[i].value(1)
-    sleep(0.5)
-    getReading(5)
-    global blackArray
-    blackArray[i] = avgRead
-    ledArray[i].value(0)
-    sleep(0.5)
-  
-  
-  global balanceSet
-  balanceSet = True
+        self.balanceSet = False
+        self.avgRead = 0.0
 
-def checkColour():
-  print("checkColour")
-  sleep(5)
-  for i in range(0,3):
-    ledArray[i].value(1)
-    sleep(0.1)
-    getReading(10)  
-    global colourArray
-    colourArray[i] = avgRead      
-    greyDiff = whiteArray[i] - blackArray[i]  
-    print("greyDiff:")
-    print(greyDiff)
-    #the reading returned minus the lowest value divided by the possible range multiplied by 255
-    #will give us a value roughly between 0-255 representing the value for the current reflectivity
-    #(for the colour it is exposed to) of what is being scanned
-    colourArray[i] = (colourArray[i] - blackArray[i])/(greyDiff)*255 
-    ledArray[i].value(0)
-    #sleep(0.5)
-  
-def getReading(times):
-  print("getReading")
-  tally = 0
-  for i in range(0,times):
-    reading = ldr.read()
-    tally = reading + tally
-    sleep(0.05)
-  
-  global avgRead
-  avgRead = (tally)/times
-  print(avgRead)
+    def checkBalance(self):
+        if self.balanceSet == False:
+            self.setBalance()
 
+    def setBalance(self):
+        print("setBalance > white")
+        sleep(5)
+        for i in range(0, 3):
+            self.ledArray[i].value(1)
+            sleep(0.5)
+            self.getReading(5)
+            self.whiteArray[i] = self.avgRead
+            self.ledArray[i].value(0)
+            sleep(0.5)
 
-def printColour():
-  print("R = ")
-  print(max(min(int(colourArray[0]), 255), 0))
-  print("G = ")
-  print(max(min(int(colourArray[1]), 255), 0))
-  print("B = ")
-  print(max(min(int(colourArray[2]), 255), 0))
-  print("whiteArray:")
-  print(whiteArray)
-  print("blackArray:")
-  print(blackArray)
-  sleep(2)
+        print("setBalance > black")
+        sleep(5)
+        for i in range(0, 3):
+            self.ledArray[i].value(1)
+            sleep(0.5)
+            self.getReading(5)
+            self.blackArray[i] = self.avgRead
+            self.ledArray[i].value(0)
+            sleep(0.5)
 
-while True:
-  #checkBalance()
-  checkColour()
-  printColour()
+        self.balanceSet = True
 
+    def checkColour(self):
+        print("checkColour")
+        sleep(5)
+        for i in range(0, 3):
+            self.ledArray[i].value(1)
+            sleep(0.1)
+            self.getReading(10)
+            self.colourArray[i] = self.avgRead
+            greyDiff = self.whiteArray[i] - self.blackArray[i]
+            print("greyDiff: ", greyDiff)
+            # the reading returned minus the lowest value divided by the possible range multiplied by 255
+            # will give us a value roughly between 0-255 representing the value for the current reflectivity
+            # (for the colour it is exposed to) of what is being scanned
+            self.colourArray[i] = (
+                self.colourArray[i] - self.blackArray[i])/(greyDiff)*255
+            self.ledArray[i].value(0)
+            # sleep(0.5)
+
+    def getReading(self, times):
+        print("getReading")
+        tally = 0
+        for i in range(0, times):
+            reading = self.ldr.read()
+            tally = reading + tally
+            sleep(0.05)
+
+        self.avgRead = (tally)/times
+        print(self.avgRead)
+
+    def printColour(self):
+        rVal = max(min(int(self.colourArray[0]), 255), 0)
+        gVal = max(min(int(self.colourArray[1]), 255), 0)
+        bVal = max(min(int(self.colourArray[2]), 255), 0)
+        print("RGB(", rVal, ",", gVal, ",", bVal, "), whiteArr = ", self.whiteArray,
+              ", blackArr = ", max(min(int(self.colourArray[2]), 255), 0))
+        sleep(2)
+
+    def run(self):
+        while True:
+            # self.checkBalance()
+            self.checkColour()
+            self.printColour()
--- light-control/light/drivers/ring_led.py
+++ light-control/light/drivers/ring_led.py
@@ -3,74 +3,72 @@ from machine import Pin
 from time import sleep
 import random
 
+
 class RING_LED():
 
-  # number of leds
-  LED_COUNT=8
-  # connected Pin
-  p=0
+    # number of leds
+    LED_COUNT = 8
+    # connected Pin
+    p = 0
 
-  def __init__(self, pin = 0):
-    self.np = neopixel.NeoPixel(Pin(pin),self.LED_COUNT)
+    def __init__(self, pin=0):
+        self.np = neopixel.NeoPixel(Pin(pin), self.LED_COUNT)
 
-  # colors the led at position in (r,g,b)
-  def color(self,position, r,g,b):
-    self.np[position]=(r,g,b)
-    self.np.write()
+    # colors the led at position in (r,g,b)
+    def color(self, position, r, g, b):
+        self.np[position] = (r, g, b)
+        self.np.write()
 
-  # remove all colors
-  def clearAll(self):
-    for i in range(self.LED_COUNT):
-      self.color(i,0,0,0)
+    # remove all colors
+    def clearAll(self):
+        for i in range(self.LED_COUNT):
+            self.color(i, 0, 0, 0)
 
-  # remove color from led at position
-  def clear(self, position):
-    self.color(position,0,0,0)
+    # remove color from led at position
+    def clear(self, position):
+        self.color(position, 0, 0, 0)
 
-  def colorAll(self,r,g,b):
-    for i in range(self.LED_COUNT):
-      # color the current led
-      self.color(i,r,g,b)
-        
-  # the color (r,g,b) should run x times like a cycle 
-  def cycleColors(self, wait, x): 
-    # before setting new colors, clear the ring
-    self.clearAll()
-    # the cycle should run x times
-    for j in range(x):
-      for i in range(self.LED_COUNT):
-        if(i==0):
-          self.clear(self.LED_COUNT-1)
-        else:
-          self.clear(i-1)
+    def colorAll(self, r, g, b):
+        for i in range(self.LED_COUNT):
+            # color the current led
+            self.color(i, r, g, b)
 
-        r=random.randint(150, 255)
-        g=random.randint(0, 100)
-        b=random.randint(50, 200)
-        # color the current led
-        self.color(i,r,g,b)
-        
-        # wait, color is displayed this time
-        sleep(wait)
+    # the color (r,g,b) should run x times like a cycle
+    def cycleColors(self, wait, x):
+        # before setting new colors, clear the ring
+        self.clearAll()
+        # the cycle should run x times
+        for j in range(x):
+            for i in range(self.LED_COUNT):
+                if(i == 0):
+                    self.clear(self.LED_COUNT-1)
+                else:
+                    self.clear(i-1)
 
+                r = random.randint(150, 255)
+                g = random.randint(0, 100)
+                b = random.randint(50, 200)
+                # color the current led
+                self.color(i, r, g, b)
 
-  # the color (r,g,b) should run x times like a cycle 
-  def cycle(self, r,g,b,wait,x): 
-    # before setting new colors, clear the ring
-    self.clearAll()
-    # the cycle should run x times
-    for j in range(x):
-      for i in range(self.LED_COUNT):
-        # clear the previous led
-        if(i==0):
-          self.clear(self.LED_COUNT-1)
-        else:
-          self.clear(i-1)
-        
-        # color the current led
-        self.color(i,r,g,b)
-        
-        # wait, color is displayed this time
-        sleep(wait)
+                # wait, color is displayed this time
+                sleep(wait)
 
+    # the color (r,g,b) should run x times like a cycle
+    def cycle(self, r, g, b, wait, x):
+        # before setting new colors, clear the ring
+        self.clearAll()
+        # the cycle should run x times
+        for j in range(x):
+            for i in range(self.LED_COUNT):
+                # clear the previous led
+                if(i == 0):
+                    self.clear(self.LED_COUNT-1)
+                else:
+                    self.clear(i-1)
 
+                # color the current led
+                self.color(i, r, g, b)
+
+                # wait, color is displayed this time
+                sleep(wait)
