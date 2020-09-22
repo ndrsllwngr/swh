b0396afe1e2d295182914f87f4814169da64e7ec
Aline-Jannika <aline_neumann@arcor.de>
2020-09-22 14:25:01 +0200

    v2

--- colour_sensor.py
+++ colour_sensor.py
@@ -1,24 +1,23 @@
 from machine import Pin, ADC
 from time import sleep
+import math
 
 ldr = ADC(Pin(35))
 ldr.atten(ADC.ATTN_11DB)
 
-ledGreen = Pin(12, Pin.OUT)
-ledRed = Pin(13, Pin.OUT)
+ledRed = Pin(12, Pin.OUT)
+ledGreen = Pin(13, Pin.OUT)
 ledBlue = Pin(14, Pin.OUT)
 
-ledArray = [ledGreen,ledRed,ledBlue]
+ledArray = [ledRed, ledGreen, ledBlue]
 colourArray = [0,0,0]
-whiteArray = [0,0,0]
-blackArray = [0,0,0]
+#whiteArray = [0,0,0]
+whiteArray = [2141, 3246, 2536]
+#blackArray = [0,0,0]
+blackArray = [654, 1622, 1239]
 
 balanceSet = False
 avgRead = 0.0
-
-#green = 0
-#red = 0
-#blue = 0
 
 def checkBalance():
   if balanceSet == False:
@@ -26,51 +25,50 @@ def checkBalance():
   
 
 def setBalance():
-  sleep(1)
   print("setBalance - white")
+  sleep(5)
   for i in range(0,3):
     ledArray[i].value(1)
-    sleep(1)
+    sleep(0.5)
     getReading(5) 
     global whiteArray
     whiteArray[i] = avgRead
     ledArray[i].value(0)
-    sleep(1)
+    sleep(0.5)
   
-  sleep(5)             
-
   print("setBalance - black")
+  sleep(5)
   for i in range(0,3):
     ledArray[i].value(1)
-    sleep(1)
+    sleep(0.5)
     getReading(5)
     global blackArray
     blackArray[i] = avgRead
-    #blackArray[i] = analogRead(2)
     ledArray[i].value(0)
-    sleep(1)
+    sleep(0.5)
   
   
   global balanceSet
   balanceSet = True
 
-  sleep(5)
-
-  
 def checkColour():
   print("checkColour")
+  sleep(5)
   for i in range(0,3):
     ledArray[i].value(1)
-    sleep(1)                    
-    getReading(5)  
+    sleep(0.1)
+    getReading(10)  
     global colourArray
     colourArray[i] = avgRead      
     greyDiff = whiteArray[i] - blackArray[i]  
     print("greyDiff:")
     print(greyDiff)
-    colourArray[i] = (colourArray[i] - blackArray[i])/(greyDiff)*255 #the reading returned minus the lowest value divided by the possible range multiplied by 255 will give us a value roughly between 0-255 representing the value for the current reflectivity(for the colour it is exposed to) of what is being scanned
+    #the reading returned minus the lowest value divided by the possible range multiplied by 255
+    #will give us a value roughly between 0-255 representing the value for the current reflectivity
+    #(for the colour it is exposed to) of what is being scanned
+    colourArray[i] = (colourArray[i] - blackArray[i])/(greyDiff)*255 
     ledArray[i].value(0)
-    sleep(1)
+    #sleep(0.5)
   
 def getReading(times):
   print("getReading")
@@ -78,7 +76,7 @@ def getReading(times):
   for i in range(0,times):
     reading = ldr.read()
     tally = reading + tally
-    sleep(1)
+    sleep(0.05)
   
   global avgRead
   avgRead = (tally)/times
@@ -87,15 +85,19 @@ def getReading(times):
 
 def printColour():
   print("R = ")
-  print(int(colourArray[1]))
+  print(max(min(int(colourArray[0]), 255), 0))
   print("G = ")
-  print(int(colourArray[0]))
+  print(max(min(int(colourArray[1]), 255), 0))
   print("B = ")
-  print(int(colourArray[2]))
+  print(max(min(int(colourArray[2]), 255), 0))
+  print("whiteArray:")
+  print(whiteArray)
+  print("blackArray:")
+  print(blackArray)
   sleep(2)
 
 while True:
-  checkBalance()
+  #checkBalance()
   checkColour()
   printColour()
 
