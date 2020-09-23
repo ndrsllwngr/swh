3154bc2dc464bb1079769f6a6fc38a52b4ad63b4
griase94 <andy.griesbeck@gmail.com>
2020-09-23 10:49:32 +0200

    Added error handling to lamp main

--- light-control/light/main.py
+++ light-control/light/main.py
@@ -12,21 +12,25 @@ initNet("Wu-Tang-Lan", "doppelhure69")
 lastColorStr = "0-0-0"
 
 while True:
-
-    reset = getNetVar("lampReset")
-    if reset == 'True':
-        setNetVar("lampReset", False)
-        import machine
-        machine.reset()
-
-    position = float(getNetVar("lampPosition"))
-    servo.rotate(position)
+    try:
+        reset = getNetVar("lampReset")
+        if reset == 'True':
+            setNetVar("lampReset", False)
+            import machine
+            machine.reset()
     
-    colors = stringToInt(getNetVar("lampColour"))
-    colorStr = str(colors)
-    print("Pos: "+str(position)+" Color: "+colorStr)
-    if colorStr != lastColorStr:
-        ring_led.colorAll(colors[0], colors[1], colors[2])
-        print("New Color: "+colorStr)
-        lastColorStr = colorStr
-    sleep_ms(10)
+        position = float(getNetVar("lampPosition"))
+        servo.rotate(position)
+        
+        colors = stringToInt(getNetVar("lampColour"))
+        colorStr = str(colors)
+        print("Pos: "+str(position)+" Color: "+colorStr)
+        if colorStr != lastColorStr:
+            ring_led.colorAll(colors[0], colors[1], colors[2])
+            print("New Color: "+colorStr)
+            lastColorStr = colorStr
+        sleep_ms(10)
+    except OSError :
+        print("Caught an OSError")
+        continue
+
