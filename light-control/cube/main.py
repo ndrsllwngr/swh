2a0c922902d3cfc7ab6941bf3ddbd2493607c2d6
griase94 <andy.griesbeck@gmail.com>
2020-09-23 10:30:07 +0200

    Move firmware into main.py & remove it

--- light-control/cube/main.py
+++ light-control/cube/main.py
@@ -1,4 +1,30 @@
-from drivers.vibmotor import VibMotor
+import os
 
-myVibmotor = VibMotor()
-myVibmotor.start_vibration()
+from drivers.mpu6050 import MPU6050_GYRO
+from drivers.colour_sensor import DIY_COLOUR_SENSOR
+from drivers.switch import SWITCH
+from drivers.speaker import SPEAKER
+from time import sleep_ms
+from util.netvars import initNet, setNetVar
+
+initNet("Wu-Tang-Lan", "doppelhure69")
+
+gyro = MPU6050_GYRO()
+switch = SWITCH(26)
+colour_sensor = DIY_COLOUR_SENSOR()
+speaker = SPEAKER()
+
+while True:
+    try:
+        gyro_pos = gyro.update_position()
+        setNetVar("lampPosition", gyro_pos)
+        if not switch.getValue():
+            speaker.beep_n(2)
+            print("COLOR_SCAN_MODE")
+            color_string = colour_sensor.checkColour()
+            setNetVar("lampColour", color_string)
+            speaker.beep_long(tone='d')
+            sleep_ms(500)
+        sleep_ms(20)
+    except KeyboardInterrupt:
+        break
