96b599886860e7bc71cf1e0e886ec4cb9a6ba3cc
Max <max.rawlinger@googlemail.com>
2020-09-23 18:42:33 +0200

    Add retry mechanism to light

--- light-control/light/main.py
+++ light-control/light/main.py
@@ -9,49 +9,63 @@ from util.colour import stringToInt
 
 servo = LAMP_SERVO()
 ring_led = RING_LED()
+lastUpdated = utime.time()
 
 initNet("Wu-Tang-Lan", "doppelhure69")
 
 cube_ip = getNetVar("cubeIP")
 
-s = socket.socket()
-s.connect((cube_ip, 9420))
 
 while True:
     try:
-        socket_data = s.readline()
-        socket_data_str = str(socket_data, 'utf8').rstrip()
-        print("Received from socket: "+socket_data_str)
-
-        if socket_data_str == "RESET":
-            print("Reset triggered...")
-            import machine
-            machine.reset()
-
-        colors_str = socket_data_str.split("$")[0]
-        colors = stringToInt(colors_str)
-        print("ColorStr: "+colors_str)
-
-        pos_str = socket_data_str.split("$")[1]
-        print("PosStr: "+pos_str)
-        position = float(pos_str)
-
-        servo.rotate(position)
-        colorStr = str(colors)
-
-        print("Pos: "+str(position)+" Color: "+colorStr)
-        ring_led.colorAll(colors[0], colors[1], colors[2])
-        sleep_ms(30)
-    except OSError:
-        print("Caught an OSError trying to reconnect socket")
-        s.close()
         s = socket.socket()
+        s.settimeout(10.0)
         s.connect((cube_ip, 9420))
+    except OSError:
+        print("No cube found, retrying.")
+        sleep_ms(1000)
         continue
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
-        s.close()
         break
+
+    while True:
+        try:
+            timeDiff = utime.time() - lastUpdated
+            if timeDiff > 1:
+                s.send(bytes('hello', 'UTF-8'))
+                lastUpdated = utime.time()
+            socket_data = s.readline()
+            socket_data_str = str(socket_data, 'utf8').rstrip()
+            print("Received from socket: "+socket_data_str)
+
+            if socket_data_str == "RESET":
+                print("Reset triggered...")
+                import machine
+                machine.reset()
+
+            colors_str = socket_data_str.split("$")[0]
+            colors = stringToInt(colors_str)
+            print("ColorStr: "+colors_str)
+
+            pos_str = socket_data_str.split("$")[1]
+            print("PosStr: "+pos_str)
+            position = float(pos_str)
+
+            servo.rotate(position)
+            colorStr = str(colors)
+
+            print("Pos: "+str(position)+" Color: "+colorStr)
+            ring_led.colorAll(colors[0], colors[1], colors[2])
+            sleep_ms(30)
+        except OSError:
+            print("Caught an OSError trying to reconnect socket")
+            s.close()
+            break
+        except KeyboardInterrupt:
+            print("KeyboardInterrupt")
+            s.close()
+            break
 
 print("s.close()")
 s.close()
