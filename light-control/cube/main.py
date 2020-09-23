74ffd7aecc59647bea44da6f065d33bc3ec59234
griase94 <andy.griesbeck@gmail.com>
2020-09-23 10:41:40 +0200

    Added reset to main.py files

--- light-control/cube/main.py
+++ light-control/cube/main.py
@@ -5,7 +5,7 @@ from drivers.colour_sensor import DIY_COLOUR_SENSOR
 from drivers.switch import SWITCH
 from drivers.speaker import SPEAKER
 from time import sleep_ms
-from util.netvars import initNet, setNetVar
+from util.netvars import initNet, setNetVar, getNetVar
 
 initNet("Wu-Tang-Lan", "doppelhure69")
 
@@ -15,9 +15,12 @@ colour_sensor = DIY_COLOUR_SENSOR()
 speaker = SPEAKER()
 
 while True:
+    reset = getNetVar("cubeReset")
+    if reset == 'True':
+        setNetVar("cubeReset", False)
+        import machine
+        machine.reset()
     try:
-        gyro_pos = gyro.update_position()
-        setNetVar("lampPosition", gyro_pos)
         if not switch.getValue():
             speaker.beep_n(2)
             print("COLOR_SCAN_MODE")
@@ -25,6 +28,9 @@ while True:
             setNetVar("lampColour", color_string)
             speaker.beep_long(tone='d')
             sleep_ms(500)
-        sleep_ms(20)
+        else:
+            gyro_pos = gyro.update_position()
+            setNetVar("lampPosition", gyro_pos)
+            sleep_ms(20)
     except KeyboardInterrupt:
         break
--- light-control/light/main.py
+++ light-control/light/main.py
@@ -1,16 +1,9 @@
-import os
-
 from drivers.servo import LAMP_SERVO
 from machine import Pin, PWM, ADC
 from time import sleep_ms
 from drivers.ring_led import RING_LED
-from util.netvars import initNet, getNetVar
+from util.netvars import initNet, getNetVar, setNetVar
 from util.colour import stringToInt
-
-analogPin = ADC(Pin(34))
-analogPin.atten(ADC.ATTN_11DB)
-
-MAX_ANALOG_VAL = 4095
 
 servo = LAMP_SERVO()
 ring_led = RING_LED()
@@ -19,12 +12,16 @@ initNet("Wu-Tang-Lan", "doppelhure69")
 lastColorStr = "0-0-0"
 
 while True:
-    #analogVal = analogPin.read()
-    #rel_val = analogVal/MAX_ANALOG_VAL
-    #print("Analog: "+str(analogVal)+" - rel: "+str(rel_val))
+
+    reset = getNetVar("lampReset")
+    if reset == 'True':
+        setNetVar("lampReset", False)
+        import machine
+        machine.reset()
+
     position = float(getNetVar("lampPosition"))
     servo.rotate(position)
-
+    
     colors = stringToInt(getNetVar("lampColour"))
     colorStr = str(colors)
     print("Pos: "+str(position)+" Color: "+colorStr)
