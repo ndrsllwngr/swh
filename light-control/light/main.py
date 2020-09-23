ad316246032dcbecbe6aeb763e121ee58ade9b0b
Max <max.rawlinger@googlemail.com>
2020-09-23 11:29:20 +0200

    Add delay for network reset (30s)

--- light-control/light/main.py
+++ light-control/light/main.py
@@ -1,4 +1,5 @@
 from drivers.servo import LAMP_SERVO
+import utime
 from machine import Pin, PWM, ADC
 from time import sleep_ms
 from drivers.ring_led import RING_LED
@@ -10,26 +11,31 @@ ring_led = RING_LED()
 
 initNet("Wu-Tang-Lan", "doppelhure69")
 lastColorStr = "0-0-0"
+lastUpdated = utime.time()
 
 while True:
     try:
-        reset = getNetVar("lampReset")
-        if reset == 'True':
-            setNetVar("lampReset", False)
-            import machine
-            machine.reset()
+        timeDiff = utime.time() - lastUpdated
+        if timeDiff > 30:
+            lastUpdated = utime.time()
+            print("OUCH!")
+            reset = getNetVar("lampReset")
+            if reset == 'True':
+                setNetVar("lampReset", False)
+                import machine
+                machine.reset()
     
         position = float(getNetVar("lampPosition"))
         servo.rotate(position)
         
         colors = stringToInt(getNetVar("lampColour"))
         colorStr = str(colors)
-        print("Pos: "+str(position)+" Color: "+colorStr)
+        print("Pos: "+str(position)+" Color: "+colorStr+" TimeDiff: "+str(timeDiff))
         if colorStr != lastColorStr:
             ring_led.colorAll(colors[0], colors[1], colors[2])
             print("New Color: "+colorStr)
             lastColorStr = colorStr
-        sleep_ms(10)
+        sleep_ms(30)
     except OSError :
         print("Caught an OSError")
         continue
